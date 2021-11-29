83f5c9b018a2a2da17421ad622f8f9eb7343d531
Dr. Tobias Quathamer <toddy@debian.org>
2021-11-29 21:58:06 +0100

    Add ServiceProvider

---
+++ src/NaiveBayesServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Toddy15\NaiveBayes;
+
+use Illuminate\Support\ServiceProvider;
+
+class NaiveBayesServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
