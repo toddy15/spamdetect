775323d131fc5b98959a41e69926d2ae368939bb
Dr. Tobias Quathamer <toddy@debian.org>
2021-12-05 21:18:50 +0100

    Initial implementation of 'classify' function

--- src/SpamDetect.php
+++ src/SpamDetect.php
@@ -6,5 +6,16 @@ namespace Toddy15\SpamDetect;
 
 class SpamDetect
 {
-
+    /**
+     * Classify a text.
+     *
+     * The function return a float value between 0.0 and 1.0,
+     * representing the likelihood of the text being ham or spam.
+     * Lower values towards 0.0 indicate 'ham', higher values
+     * towards 1.0 indicate 'spam'.
+     */
+    public function classify(string $string): float
+    {
+        return 0.5;
+    }
 }
--- tests/Feature/SpamDetectTest.php
+++ tests/Feature/SpamDetectTest.php
@@ -8,3 +8,9 @@ it('can instantiate a spam detector', function () {
     $spamdetect = new SpamDetect();
     expect($spamdetect)->toBeInstanceOf(SpamDetect::class);
 });
+
+it('classifies a text with an empty database as 0.5', function () {
+    $spamdetect = new SpamDetect();
+    $result = $spamdetect->classify('This text should rate with 0.5');
+    expect($result)->toBe(0.5);
+});
